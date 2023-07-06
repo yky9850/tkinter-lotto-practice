8d12acc7872249774990bae3c3ab15983cd29320
DESKTOP-V1OMAP2\202-4 <rbghks4677@naver.com>
2023-07-06 12:55:52 +0900

    tkinter-lotto

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import tkinter
 import tkinter.font
-
 import random
 
 lotto_num = range(1,46)
 
 def buttonClick():
-    print(random.sample(lotto_num,6))
+    label = tkinter.Label(window, text=str(random.sample(lotto_num,6)))
+    label.pack()
     
 window=tkinter.Tk()
 window.title("lotto")
